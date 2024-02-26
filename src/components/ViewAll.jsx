0096e7adabd30658d93d3722d230d84406a64d5f
sumitjambharkar <sujam3545@gmail.com>
2024-02-26 16:07:26 +0530

    add

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -11,37 +11,55 @@ const ViewAll = () => {
 
   return (
     <>
-    <div className="container_banner">
-    <div className="collect_sec colle_1">
-        <span className="span_coll">Popular Categories</span>
-        <h2>COLLECTIONS</h2>
-        <span className="spn_de">Eliminate the hassle of clinic waits, pharmacy lines, and uncomfortable discussions.</span>
-        </div>
-      <div className="collectin_seci">
-       
-        <div className="collect_sec colle_2">
-       
-        <h2><Link to="/product-category/acne-care">Acne Care</Link></h2>
-        <span className="spn_an"><Link to="/product-category/acne-care">SEE MORE</Link></span>
-        
-        </div>
-        <div className="collect_sec colle_3">
-  
-        <h2><Link to="/product-category/skin-care">Beauty and Skin Care</Link></h2>
-        <span className="spn_an"><Link to="/product-category/skin-care">SEE MORE</Link></span>
+      <div className="container_banner">
+
+        <div className="collectin_seci">
+          <div className="collect_sec colle_1">
+            <span className="span_coll">Popular Categories</span>
+            <h2>COLLECTIONS</h2>
+            <span className="spn_de">Eliminate the hassle of clinic waits, pharmacy lines, and uncomfortable discussions.</span>
+          </div>
+
+
+          <div className="collect_sec colle_3">
+
+            <h2><Link to="/product-category/skin-care">Beauty and Skin Care</Link></h2>
+            <span className="spn_an"><Link to="/product-category/skin-care">SEE MORE</Link></span>
+
+          </div>
+          
+          <div className="collect_sec colle_2">
+
+            <h2><Link to="/product-category/acne-care">Acne Care</Link></h2>
+            <span className="spn_an"><Link to="/product-category/acne-care">SEE MORE</Link></span>
+
+          </div>
+          <div className="collect_sec colle_4">
+
+            <h2><Link to="/product-category/eye-care">Eye Care</Link></h2>
+            <span className="spn_an"><Link to="/product-category/eye-care">SEE MORE</Link></span>
+
+          </div>
+          <div className="collect_sec colle_6">
+
+            <h2><Link to="/product-category/nootropics">Nootropics</Link></h2>
+            <span className="spn_an"><Link to="/product-category/nootropics">SEE MORE</Link></span>
+
+          </div>
+
+          <div className="collect_sec colle_7">
+
+            <h2><Link to="/product-category/hair-care">Hair Care</Link></h2>
+            <span className="spn_an"><Link to="/product-category/hair-care">SEE MORE</Link></span>
+
+          </div>
+          
+
+
 
         </div>
-        <div className="collect_sec colle_4">
-     
-        <h2><Link to="/product-category/eye-care">Eye Care</Link></h2>
-        <span className="spn_an"><Link to="/product-category/eye-care">SEE MORE</Link></span>
-
-        </div>
-        
-       
-        </div>
-    </div>
-</>
+      </div>
+    </>
   );
 };
 
